4d0dcba6f08e93afb773d207bbd6945075cda53f
mohammadJzade <125662447+mohammad1384javadi@users.noreply.github.com>
2025-03-07 15:16:09 +0330

    Class Tamrin

---
+++ 02/02d.js
@@ -0,0 +1,11 @@
+let number = process.argv.slice(3);
+let command = process.argv[2];
+if (command === "sum"){
+    console.log(Number(number[0]) + Number(number[1]));
+}
+else if (command === "minus"){
+    console.log(Number(number[0]) - Number(number[1]))
+}
+else{
+    console.log("Command not found.")
+}
